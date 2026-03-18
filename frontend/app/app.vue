09915933604e0a72644046779a977536bdeb0d27
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2026-03-19 00:15:47 +0700

    chore: add shadcn-ui

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+
+const counter = ref(0);
+
+const handleClick = () => {
+  counter.value++;
+};
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <Button class="bg-gray-500" @click="handleClick">Click Me</Button>
+    <p>Count: {{ counter }}</p>
   </div>
 </template>
